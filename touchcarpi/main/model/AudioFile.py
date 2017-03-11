554663535e2839b95198d993d18a9c61c316c3de
Plata17 <pl4t417@gmail.com>
2017-03-11 15:07:18 +0100

    Added AudioStatus enum class to define reproduction statuses in the AudioFile class. Some changes on AudioFile class.

--- touchcarpi/main/model/AudioFile.py
+++ touchcarpi/main/model/AudioFile.py
@@ -1,22 +1,27 @@
 
 
 from .AudioFileMP3 import AudioFileMP3
+from.AudioStatus import AudioStatus
 
 class AudioFile:
 
     def __init__(self):
         self.path = ""
         self.savedSecond = 0
-        self.status = False
+        self.status = AudioStatus.NOFILE
         self.audioFileObject = None
 
     def playAudio(self, path):
         self.path = path
-        if(self.status == False):
-            if (self.path.endswith(".mp3")):
-                self.audioFileObject = AudioFileMP3()
-            self.status = True
-        self.audioFileObject.playAudio(self.path)
+        if(self.status == AudioStatus.NOFILE):
+            self.audioFileObject = self.__selectAudioType(self.path)
+            self.audioFileObject.playAudio(self.path)
+            self.status = AudioStatus.PLAYING
+
+        elif (self.status == AudioStatus.PLAYING):
+            self.audioFileObject.stopAudio()
+            self.audioFileObject = self.__selectAudioType(self.path)
+            self.audioFileObject.playAudio(self.path)
 
     def pauseAudio(self):
         self.audioFileObject.pauseAudio()
@@ -25,9 +30,15 @@ class AudioFile:
         self.audioFileObject.reanudeAudio(savedSecond)
 
     def stopAudio(self):
-        self.status = False
+        self.status = AudioStatus.NOFILE
         self.audioFileObject.stopAudio()
         self.audioFileObject = None
 
     def getPath(self):
         return self.path
+
+    def __selectAudioType(self, path):
+        if (self.path.endswith(".mp3")):
+            audioType = AudioFileMP3()
+
+        return audioType
---
+++ touchcarpi/main/model/AudioStatus.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class AudioStatus(Enum):
+    NOFILE = 1
+    PLAYING = 2
+    PAUSED = 3
